a8c40e9e825d156e529886ee323d11f2a1dbacbf
Daniel Melemed <danny@melemed.com>
2016-10-01 09:51:53 -0400

    added heroku config + extra configs

---
+++ src/main/java/com/feedme/LoggingAspectConfiguration.java
@@ -0,0 +1,19 @@
+package com.feedme;
+
+import com.feedme.aop.logging.LoggingAspect;
+import com.feedme.config.Constants;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(Constants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
+}
